cab3f9388c45f6e2432d6e3735c641cb9a649a25
RedFeuer <danilabespyatov@gmail.com>
2026-05-09 13:16:23 +0300

    ADD: useCases to get ProductsPage and ProductDetails

---
+++ app/src/main/java/com/example/productsStore/domain/useCase/GetProductsPageUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.productsStore.domain.useCase
+
+import com.example.productsStore.domain.model.ProductsPageModel
+import com.example.productsStore.domain.repository.ProductsRepository
+
+class GetProductsPageUseCase(
+    private val productsRepository: ProductsRepository,
+) {
+    suspend operator fun invoke(limit : Int, skip : Int) : ProductsPageModel {
+        return productsRepository.getProductsPage(
+            limit = limit,
+            skip = skip,
+        )
+    }
+}
