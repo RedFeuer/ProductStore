200c718521c1a9257aa6672adb2480df2189816c
RedFeuer <danilabespyatov@gmail.com>
2026-05-09 10:16:35 +0300

    ADD: separated screen to be Navigation's root

---
+++ app/src/main/java/com/example/productsStore/presentation/AppRoot.kt
@@ -0,0 +1,8 @@
+package com.example.productsStore.presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppRoot() {
+    
+}
---
+++ app/src/main/java/com/example/productsStore/presentation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.productsStore.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+//тут будешь ваша активити
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppRoot()
+        }
+    }
+}
