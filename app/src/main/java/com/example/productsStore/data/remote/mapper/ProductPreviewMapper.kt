c43ed856d0872d4a9ea18d2a2ed627c1bf28e8cd
RedFeuer <danilabespyatov@gmail.com>
2026-05-09 12:28:00 +0300

    ADD: Mappers: DomainModel - DTO

---
+++ app/src/main/java/com/example/productsStore/data/remote/mapper/ProductPreviewMapper.kt
@@ -0,0 +1,24 @@
+package com.example.productsStore.data.remote.mapper
+
+import com.example.productsStore.data.remote.dto.ProductPreviewDto
+import com.example.productsStore.domain.model.ProductPreviewModel
+
+class ProductPreviewMapper {
+    fun toDomainModel(previewDto: ProductPreviewDto) : ProductPreviewModel {
+        return ProductPreviewModel(
+            id = previewDto.id,
+            title = previewDto.title,
+            price = previewDto.price,
+            brand =  previewDto.brand,
+        )
+    }
+
+    fun toDto(previewModel : ProductPreviewModel) : ProductPreviewDto {
+        return ProductPreviewDto(
+            id = previewModel.id,
+            title = previewModel.title,
+            price = previewModel.price,
+            brand =  previewModel.brand,
+        )
+    }
+}
---
+++ app/src/main/java/com/example/productsStore/data/remote/mapper/ProductsPageMapper.kt
@@ -0,0 +1,30 @@
+package com.example.productsStore.data.remote.mapper
+
+import com.example.productsStore.data.remote.dto.ProductsPageDto
+import com.example.productsStore.domain.model.ProductsPageModel
+
+class ProductsPageMapper (
+    private val productPreviewMapper: ProductPreviewMapper,
+) {
+    fun toDomainModel(productsDto : ProductsPageDto) : ProductsPageModel {
+        return ProductsPageModel(
+            products = productsDto.products.map { productPreviewDto ->
+                productPreviewMapper.toDomainModel(productPreviewDto)
+            },
+            total = productsDto.total,
+            skip = productsDto.skip,
+            limit = productsDto.limit,
+        )
+    }
+
+    fun toDto(productsModel : ProductsPageModel) : ProductsPageDto {
+        return ProductsPageDto(
+            products = productsModel.products.map { productPreviewModel ->
+                productPreviewMapper.toDto(productPreviewModel)
+            },
+            total = productsModel.total,
+            skip = productsModel.skip,
+            limit = productsModel.limit,
+        )
+    }
+}
