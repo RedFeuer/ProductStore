84b4f7fddb8668378b9b86aed8e3eccd4facc697
RedFeuer <danilabespyatov@gmail.com>
2026-05-09 12:28:00 +0300

    ADD: Mappers: DomainModel - DTO

---
+++ app/src/main/java/com/example/productsStore/data/remote/mapper/ProductDetailsMapper.kt
@@ -0,0 +1,34 @@
+package com.example.productsStore.data.remote.mapper
+
+import com.example.productsStore.data.remote.dto.ProductDetailsDto
+import com.example.productsStore.domain.model.ProductDetailsModel
+
+class ProductDetailsMapper {
+    fun toDomainModel(detailsDto : ProductDetailsDto) : ProductDetailsModel {
+        return ProductDetailsModel(
+            id = detailsDto.id,
+            title = detailsDto.title,
+            price = detailsDto.price,
+            brand =  detailsDto.brand,
+            description = detailsDto.description,
+            rating = detailsDto.rating,
+            weight = detailsDto.weight,
+            availabilityStatus = detailsDto.availabilityStatus,
+            warrantyInformation = detailsDto.warrantyInformation,
+        )
+    }
+
+    fun toDto(detailsModel : ProductDetailsModel) : ProductDetailsDto {
+        return ProductDetailsDto(
+            id = detailsModel.id,
+            title = detailsModel.title,
+            price = detailsModel.price,
+            brand =  detailsModel.brand,
+            description = detailsModel.description,
+            rating = detailsModel.rating,
+            weight = detailsModel.weight,
+            availabilityStatus = detailsModel.availabilityStatus,
+            warrantyInformation = detailsModel.warrantyInformation,
+        )
+    }
+}
