07a2654d460750b0d214126c8e92cc4ecdf2d705
RedFeuer <danilabespyatov@gmail.com>
2026-05-10 19:38:57 +0300

    IMPROVE: docs

--- app/src/main/java/com/example/productsStore/data/remote/dto/ProductsPageDto.kt
+++ app/src/main/java/com/example/productsStore/data/remote/dto/ProductsPageDto.kt
@@ -5,8 +5,12 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ProductsPageDto(
     val products: List<ProductPreviewDto>,
+    /** общее количество заметок на сервер */
     val total: Int,
+    /** сколько элементов пропускаем от начала список
+     * аналогично offset при работе с IO */
     val skip: Int,
+    /** сколько элементов сервер вернет за один запрос */
     val limit: Int,
 ) {
 }
--- app/src/main/java/com/example/productsStore/domain/model/ProductsPageModel.kt
+++ app/src/main/java/com/example/productsStore/domain/model/ProductsPageModel.kt
@@ -2,7 +2,11 @@ package com.example.productsStore.domain.model
 
 data class ProductsPageModel(
     val products: List<ProductPreviewModel>,
+    /** общее количество заметок на сервер */
     val total: Int,
+    /** сколько элементов пропускаем от начала список
+     * аналогично offset при работе с IO */
     val skip: Int,
+    /** сколько элементов сервер вернет за один запрос */
     val limit: Int,
 )
