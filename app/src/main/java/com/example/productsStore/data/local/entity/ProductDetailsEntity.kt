2b2a28eca4cf7d1c42ce868b4a2201fc5a4ac447
RedFeuer <danilabespyatov@gmail.com>
2026-05-12 20:01:10 +0300

    ADD: Product Preview and Details Entities

---
+++ app/src/main/java/com/example/productsStore/data/local/entity/ProductDetailsEntity.kt
@@ -0,0 +1,19 @@
+package com.example.productsStore.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "product_details")
+data class ProductDetailsEntity(
+    @PrimaryKey
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val brand: String?,
+
+    val description: String,
+    val rating: Double,
+    val weight: Int,
+    val availabilityStatus: String, // можно потом отдельным классом сделать, в API два состояния всего
+    val warrantyInformation: String,
+)
