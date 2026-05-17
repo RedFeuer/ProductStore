4edb37800088e1a8ad046ade6f05dc5c3edcc1d7
RedFeuer <danilabespyatov@gmail.com>
2026-05-17 15:10:53 +0300

    IMPROVE: repository and implementation

--- app/src/main/java/com/example/productsStore/domain/repository/ProductsRepository.kt
+++ app/src/main/java/com/example/productsStore/domain/repository/ProductsRepository.kt
@@ -1,6 +1,7 @@
 package com.example.productsStore.domain.repository
 
-import com.example.productsStore.domain.model.ProductDetailsModel
+import com.example.productsStore.domain.model.CachedProductDetailsModel
+import com.example.productsStore.domain.model.CartProductModel
 import com.example.productsStore.domain.model.ProductPreviewModel
 import com.example.productsStore.domain.model.ProductsPageModel
 import kotlinx.coroutines.flow.Flow
@@ -9,6 +10,14 @@ interface ProductsRepository {
     suspend fun observeProductPreviews(limit: Int) : Flow<List<ProductPreviewModel>>
     suspend fun refreshProductsPage(limit: Int, skip: Int) : ProductsPageModel
 
-    suspend fun observeProductDetails(id: Int) : Flow<ProductDetailsModel?>
-    suspend fun refreshProductDetails(id : Int) : ProductDetailsModel
+    suspend fun observeProductDetails(id: Int) : Flow<CachedProductDetailsModel?>
+    suspend fun refreshProductDetailsIfNeeded(id : Int)
+
+    suspend fun observeCartProducts() : Flow<List<CartProductModel>>
+
+    /** TODO: подумать, если потребуется добавлять в корзину через экран
+     *  конкретного товара, то надо будет маппить Details -> Preview и добавлять */
+    suspend fun addProductToCart(product: ProductPreviewModel)
+
+    suspend fun clearCart()
 }
