7ffb9228673f182d4b4f62e6d9ebbd75cbd4ab56
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-05-18 19:28:55 +0300

    FIX: ProductPreviewModel -> ProductDetailsModel in useCase and repository that adding Product to Cart

--- app/src/main/java/com/example/productsStore/domain/repository/ProductsRepository.kt
+++ app/src/main/java/com/example/productsStore/domain/repository/ProductsRepository.kt
@@ -2,6 +2,7 @@ package com.example.productsStore.domain.repository
 
 import com.example.productsStore.domain.model.CachedProductDetailsModel
 import com.example.productsStore.domain.model.CartProductModel
+import com.example.productsStore.domain.model.ProductDetailsModel
 import com.example.productsStore.domain.model.ProductPreviewModel
 import com.example.productsStore.domain.model.ProductsPageModel
 import kotlinx.coroutines.flow.Flow
@@ -15,9 +16,7 @@ interface ProductsRepository {
 
     suspend fun observeCartProducts() : Flow<List<CartProductModel>>
 
-    /** TODO: подумать, если потребуется добавлять в корзину через экран
-     *  конкретного товара, то надо будет маппить Details -> Preview и добавлять */
-    suspend fun addProductToCart(product: ProductPreviewModel)
+    suspend fun addProductToCart(product: ProductDetailsModel)
 
     suspend fun clearCart()
 }
--- app/src/main/java/com/example/productsStore/domain/useCase/AddProductToCartUseCase.kt
+++ app/src/main/java/com/example/productsStore/domain/useCase/AddProductToCartUseCase.kt
@@ -1,13 +1,13 @@
 package com.example.productsStore.domain.useCase
 
-import com.example.productsStore.domain.model.ProductPreviewModel
+import com.example.productsStore.domain.model.ProductDetailsModel
 import com.example.productsStore.domain.repository.ProductsRepository
 import javax.inject.Inject
 
 class AddProductToCartUseCase @Inject constructor(
     private val productsRepository: ProductsRepository,
 ) {
-    suspend operator fun invoke(product: ProductPreviewModel) {
+    suspend operator fun invoke(product: ProductDetailsModel) {
         productsRepository.addProductToCart(product)
     }
 }
