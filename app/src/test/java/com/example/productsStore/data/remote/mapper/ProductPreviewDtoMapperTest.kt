996066313e7058eba8a150b46957dfe4b0cdda40
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-05-19 21:12:36 +0300

    ADD: DTO -> DomainModel test of ProductPreview with null-brand

--- app/src/test/java/com/example/productsStore/data/remote/mapper/ProductPreviewDtoMapperTest.kt
+++ app/src/test/java/com/example/productsStore/data/remote/mapper/ProductPreviewDtoMapperTest.kt
@@ -2,6 +2,7 @@ package com.example.productsStore.data.remote.mapper
 
 import com.example.productsStore.data.remote.dto.ProductPreviewDto
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNull
 import org.junit.Test
 
 class ProductPreviewDtoMapperTest {
@@ -23,7 +24,29 @@ class ProductPreviewDtoMapperTest {
         // THEN
         assertEquals(10, actual.id)
         assertEquals("Red Lipstick", actual.title)
-        assertEquals(12.99, actual.price)
+        assertEquals(12.99, actual.price, 0.0)
         assertEquals("Chic Cosmetics", actual.brand)
+    }
+
+    @Test
+    fun `GIVEN product preview dto with null brand WHEN map to domain THEN return model with null brand`() {
+        // GIVEN
+        val mapper = ProductPreviewDtoMapper()
+
+        val dto = ProductPreviewDto(
+            id = 11,
+            title = "Unknown Product",
+            price = 15.0,
+            brand = null,
+        )
+
+        // WHEN
+        val actual = mapper.toDomainModel(dto)
+
+        // THEN
+        assertEquals(11, actual.id)
+        assertEquals("Unknown Product", actual.title)
+        assertEquals(15.0, actual.price, 0.0)
+        assertNull(actual.brand)
     }
 }
