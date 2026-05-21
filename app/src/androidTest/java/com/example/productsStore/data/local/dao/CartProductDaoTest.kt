a039b6c775164940d68abe654ee27b5430f1f75b
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-05-21 19:49:22 +0300

    ADD: Cart test when adding product to cart that store the same one and quantity increases

--- app/src/androidTest/java/com/example/productsStore/data/local/dao/CartProductDaoTest.kt
+++ app/src/androidTest/java/com/example/productsStore/data/local/dao/CartProductDaoTest.kt
@@ -5,13 +5,20 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.productsStore.data.local.database.ProductDatabase
 import junit.framework.TestCase
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+
+// GIVEN
+
+        // WHEN
+
+        // THEN
+
 
 @RunWith(AndroidJUnit4::class)
 class CartProductDaoTest {
@@ -64,5 +71,40 @@ class CartProductDaoTest {
         TestCase.assertEquals(price, cartProduct.price)
         TestCase.assertEquals(brand, cartProduct.brand)
         TestCase.assertEquals(1, cartProduct.quantity)
+    }
+
+    /* Повторное добавление → увеличивается количество, запись не дублируется . */
+    @Test
+    fun givenProductAlreadyInCartWhenAddSameProductThenIncreaseQuantityWithoutDuplicate() = runBlocking {
+        // GIVEN
+        val productId = 1
+        val title = "Essence Mascara Lash Princess"
+        val price = 9.99
+        val brand = "Essence"
+
+        cartProductDao.addProductToCart(
+            productId = productId,
+            title = title,
+            price = price,
+            brand = brand,
+        )
+
+        // WHEN
+        cartProductDao.addProductToCart(
+            productId = productId,
+            title = title,
+            price = price,
+            brand = brand,
+        )
+
+        val actual = cartProductDao.observeCartProducts().first()
+
+        // THEN
+        assertEquals(1, actual.size)
+
+        val cartProduct = actual.first()
+
+        assertEquals(productId, cartProduct.productId)
+        assertEquals(2, cartProduct.quantity)
     }
 }
