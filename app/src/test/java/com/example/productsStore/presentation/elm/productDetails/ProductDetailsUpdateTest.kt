7ff616aba27f975746b6f78b6bf2e181319caa2e
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-05-27 21:17:05 +0300

    ADD: error refreshing on success state test for ProductDetailsUpdate

--- app/src/test/java/com/example/productsStore/presentation/elm/productDetails/ProductDetailsUpdateTest.kt
+++ app/src/test/java/com/example/productsStore/presentation/elm/productDetails/ProductDetailsUpdateTest.kt
@@ -118,6 +118,41 @@ class ProductDetailsUpdateTest {
         assertTrue(actual.news.isEmpty())
     }
 
+    @Test
+    fun `GIVEN success state WHEN refresh failed THEN state remains success and show message news generated`() {
+        // GIVEN
+        val product = createProductDetailsModel(
+            id = PRODUCT_ID,
+        )
+
+        val initialState = ProductDetailsState(
+            productId = PRODUCT_ID,
+            contentState = ProductDetailsContentState.Success(
+                product = product,
+                isStale = true,
+            )
+        )
+
+        val event = ProductDetailsEvent.ProductDetailsRefreshingFailed(
+            message = NETWORK_ERROR_MESSAGE,
+        )
+
+        // WHEN
+        val actual = update.update(
+            state = initialState,
+            event = event,
+        )
+
+        // THEN
+        assertEquals(initialState, actual.state)
+        assertTrue(actual.commands.isEmpty())
+        assertEquals(1, actual.news.size)
+        assertEquals(
+            ProductDetailsNews.ShowMessage(message = NETWORK_ERROR_MESSAGE),
+            actual.news.first(),
+        )
+    }
+
     private fun createProductDetailsModel(
         id: Int = 1,
         title: String = "Product",
