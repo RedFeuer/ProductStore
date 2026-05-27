73f7110ad45e47856f85b96257259e72203fc08f
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-05-27 21:43:22 +0300

    IMPROVE: remove helper comment

--- app/src/test/java/com/example/productsStore/presentation/elm/productDetails/ProductDetailsUpdateTest.kt
+++ app/src/test/java/com/example/productsStore/presentation/elm/productDetails/ProductDetailsUpdateTest.kt
@@ -6,12 +6,6 @@ import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
-        // GIVEN
-
-        // WHEN
-
-        // THEN
-
 class ProductDetailsUpdateTest {
 
     private val update = ProductDetailsUpdate()
