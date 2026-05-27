78e2a883aa885d331899e9474d3c7868fea6c096
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-05-27 22:13:00 +0300

    ADD: page refreshed test for ProductsList

--- app/src/test/java/com/example/productsStore/presentation/elm/productsList/ProductsListUpdateTest.kt
+++ app/src/test/java/com/example/productsStore/presentation/elm/productsList/ProductsListUpdateTest.kt
@@ -1,6 +1,7 @@
 package com.example.productsStore.presentation.elm.productsList
 
 import com.example.productsStore.domain.model.ProductPreviewModel
+import com.example.productsStore.domain.model.ProductsPageModel
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
@@ -92,6 +93,55 @@ class ProductsListUpdateTest {
             ),
             actual.commands[1],
         )
+    }
+
+    @Test
+    fun `GIVEN page refreshed WHEN update THEN pagination fields are updated`() {
+        // GIVEN
+        val initialState = ProductsListState(
+            products = listOf(
+                createProductPreviewModel(id = 1),
+                createProductPreviewModel(id = 2),
+            ),
+            pageSize = PAGE_SIZE,
+            loadedLimit = PAGE_SIZE,
+            nextSkip = 2,
+            isPageLoading = true,
+        )
+
+        val productsPage = ProductsPageModel(
+            products = listOf(
+                createProductPreviewModel(id = 3),
+                createProductPreviewModel(id = 4),
+            ),
+            total = 10,
+            skip = 2,
+            limit = PAGE_SIZE,
+        )
+
+        val event = ProductsListEvent.PageRefreshed(
+            productsPage = productsPage,
+            isInitialLoading = false,
+        )
+
+        // WHEN
+        val actual = update.update(
+            state = initialState,
+            event = event,
+        )
+
+        // THEN
+        val actualState = requireNotNull(actual.state) {
+            "После PageRefreshed state = null"
+        }
+
+        assertEquals(10, actualState.totalProducts)
+        assertEquals(4, actualState.nextSkip)
+        assertEquals(false, actualState.isPageLoading)
+        assertEquals(null, actualState.pageErrorMessage)
+        assertEquals(false, actualState.endReached)
+        assertTrue(actual.commands.isEmpty())
+        assertTrue(actual.news.isEmpty())
     }
 
     private fun createProductPreviewModel(
