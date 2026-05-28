be865f066eb13f2cfa9792cd92c15b19ad5c7f46
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-05-28 12:52:45 +0300

    ADD: animation disabling for UI-tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    testOptions {
+        animationsDisabled = true
+    }
 }
 
 dependencies {
