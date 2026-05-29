d06d1cfd349b57732c55f44b0e1f9e2ac53de819
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-05-29 21:27:27 +0300

    IMPROVE: NetworkStateHolder by adding start and stop monitoring and validating InternetConnection

--- app/src/main/java/com/example/productsStore/network/NetworkStateHolder.kt
+++ app/src/main/java/com/example/productsStore/network/NetworkStateHolder.kt
@@ -2,6 +2,7 @@ package com.example.productsStore.network
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.Network
 import android.net.NetworkCapabilities
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,19 +18,63 @@ class NetworkStateHolder @Inject constructor(
     private val connectivityManager: ConnectivityManager =
         context.getSystemService(ConnectivityManager::class.java)
 
-    private val _isOffline = MutableStateFlow<Boolean>(!hasInternetConnection())
+    private val _isOffline = MutableStateFlow<Boolean>(!hasValidatedInternetConnection())
     val isOffline: StateFlow<Boolean> = _isOffline.asStateFlow()
 
-    fun refresh() {
-        _isOffline.value = !hasInternetConnection()
+    private var isMonitoringStarted: Boolean = false
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            refresh()
+        }
+
+        override fun onLost(network: Network) {
+            refresh()
+        }
+
+        override fun onCapabilitiesChanged(
+            network: Network,
+            networkCapabilities: NetworkCapabilities
+        ) {
+            _isOffline.value = !networkCapabilities.hasValidatedInternetConnection()
+        }
+
+        override fun onUnavailable() {
+            _isOffline.value = true
+        }
     }
 
-    private fun hasInternetConnection(): Boolean {
+    fun refresh() {
+        _isOffline.value = !hasValidatedInternetConnection()
+    }
+
+    fun startMonitoring() {
+        if (isMonitoringStarted) return
+
+        connectivityManager.registerDefaultNetworkCallback(networkCallback)
+        isMonitoringStarted = true
+
+        refresh()
+    }
+
+    fun stopMonitoring() {
+        if (!isMonitoringStarted) return
+
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+        isMonitoringStarted = false
+    }
+
+    private fun hasValidatedInternetConnection(): Boolean {
         val activeNetwork = connectivityManager.activeNetwork ?: return false
 
         val networkCapabilities =
             connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
 
-        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        return networkCapabilities.hasValidatedInternetConnection()
+    }
+
+    private fun NetworkCapabilities.hasValidatedInternetConnection(): Boolean {
+        return hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
     }
 }
