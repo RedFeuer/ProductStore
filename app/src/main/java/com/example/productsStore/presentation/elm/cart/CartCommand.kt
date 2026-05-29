0ffc01fac82d5379875c039bde89d7ba41ab38af
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-05-29 15:03:35 +0300

    IMPROVE: make product instead of productId field

--- app/src/main/java/com/example/productsStore/presentation/elm/cart/CartCommand.kt
+++ app/src/main/java/com/example/productsStore/presentation/elm/cart/CartCommand.kt
@@ -1,12 +1,14 @@
 package com.example.productsStore.presentation.elm.cart
 
+import com.example.productsStore.domain.model.CartProductModel
+
 sealed interface CartCommand {
     data object ObserveCartProducts : CartCommand
 
     data object ClearCart : CartCommand
 
     data class SetProductReminderEnabled(
-        val productId: Int,
+        val product: CartProductModel,
         val enabled: Boolean,
     ) : CartCommand
 }
