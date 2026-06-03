45f9845d5dc1ee92c8dd64370d78587c6b918e06
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-06-03 20:35:21 +0300

    ADD: DI binding for ProductDetailsRepository

--- app/src/main/java/com/example/productsStore/di/ProductModule.kt
+++ app/src/main/java/com/example/productsStore/di/ProductModule.kt
@@ -1,8 +1,10 @@
 package com.example.productsStore.di
 
 import com.example.productsStore.data.repositoryImpl.ProductsRepositoryImpl
+import com.example.productsStore.data.repositoryImpl.productDetails.ProductDetailsRepositoryImpl
 import com.example.productsStore.data.repositoryImpl.productsList.ProductsListRepositoryImpl
 import com.example.productsStore.domain.repository.ProductsRepository
+import com.example.productsStore.domain.repository.productDetails.ProductDetailsRepository
 import com.example.productsStore.domain.repository.productsList.ProductsListRepository
 import dagger.Binds
 import dagger.Module
@@ -20,4 +22,8 @@ abstract class ProductModule {
     @Binds
     @Singleton
     abstract fun bindProductsListRepository(impl: ProductsListRepositoryImpl) : ProductsListRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindProductDetailsRepository(impl: ProductDetailsRepositoryImpl) : ProductDetailsRepository
 }
