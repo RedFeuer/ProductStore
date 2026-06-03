22ba55759ef1b5770536fb74ee527d69191bd8c1
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-06-03 21:57:58 +0300

    IMPROVE: make List -> ImmutableList in UiState to decrease recompositions

--- app/src/main/java/com/example/productsStore/presentation/elm/productsList/ProductsListState.kt
+++ app/src/main/java/com/example/productsStore/presentation/elm/productsList/ProductsListState.kt
@@ -2,6 +2,7 @@ package com.example.productsStore.presentation.elm.productsList
 
 import com.example.productsStore.domain.model.ProductPreviewModel
 import com.example.productsStore.presentation.state.ProductListUiState
+import kotlinx.collections.immutable.toImmutableList
 
 data class ProductsListState(
     val products: List<ProductPreviewModel> = emptyList(),
@@ -25,7 +26,7 @@ fun ProductsListState.toUiState(): ProductListUiState {
     return when {
         products.isNotEmpty() -> {
             ProductListUiState.Success(
-                products = products,
+                products = products.toImmutableList(),
                 isPageLoading = isPageLoading,
                 pageErrorMessage = pageErrorMessage,
                 endReached = endReached,
