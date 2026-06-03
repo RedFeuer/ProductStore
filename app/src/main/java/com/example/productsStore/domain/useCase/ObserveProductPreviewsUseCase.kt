d0330a9c6565dce789f1bc8c1a7b400d9959cffd
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-06-03 20:25:08 +0300

    IMPROVE: use ProductsListRepository instead of ProductsRepository

--- app/src/main/java/com/example/productsStore/domain/useCase/ObserveProductPreviewsUseCase.kt
+++ app/src/main/java/com/example/productsStore/domain/useCase/ObserveProductPreviewsUseCase.kt
@@ -1,14 +1,14 @@
 package com.example.productsStore.domain.useCase
 
 import com.example.productsStore.domain.model.ProductPreviewModel
-import com.example.productsStore.domain.repository.ProductsRepository
+import com.example.productsStore.domain.repository.productsList.ProductsListRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ObserveProductPreviewsUseCase @Inject constructor(
-    private val productsRepository: ProductsRepository,
+    private val productsListRepository: ProductsListRepository,
 ) {
-    suspend operator fun invoke(limit: Int) : Flow<List<ProductPreviewModel>> {
-        return productsRepository.observeProductPreviews(limit)
+    suspend operator fun invoke(limit: Int): Flow<List<ProductPreviewModel>> {
+        return productsListRepository.observeProductPreviews(limit)
     }
 }
--- app/src/main/java/com/example/productsStore/domain/useCase/RefreshProductsPageUseCase.kt
+++ app/src/main/java/com/example/productsStore/domain/useCase/RefreshProductsPageUseCase.kt
@@ -1,13 +1,13 @@
 package com.example.productsStore.domain.useCase
 
 import com.example.productsStore.domain.model.ProductsPageModel
-import com.example.productsStore.domain.repository.ProductsRepository
+import com.example.productsStore.domain.repository.productsList.ProductsListRepository
 import javax.inject.Inject
 
 class RefreshProductsPageUseCase @Inject constructor(
-    private val productsRepository: ProductsRepository,
+    private val productsListRepository: ProductsListRepository,
 ) {
-    suspend operator fun invoke(limit: Int, skip: Int) : ProductsPageModel {
-        return productsRepository.refreshProductsPage(limit, skip)
+    suspend operator fun invoke(limit: Int, skip: Int): ProductsPageModel {
+        return productsListRepository.refreshProductsPage(limit, skip)
     }
 }
