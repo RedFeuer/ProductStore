388f4577fc3869b165d3d6aeee8838a497771532
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-06-05 18:33:19 +0300

    ADD: ProductDetailsMessage to UI

--- app/src/main/java/com/example/productsStore/presentation/navigation/ProductDetailsRoute.kt
+++ app/src/main/java/com/example/productsStore/presentation/navigation/ProductDetailsRoute.kt
@@ -1,14 +1,19 @@
 package com.example.productsStore.presentation.navigation
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.productsStore.presentation.elm.productDetails.ProductDetailsMessage
 import com.example.productsStore.presentation.elm.productDetails.ProductDetailsNews
 import com.example.productsStore.presentation.elm.productDetails.toUiState
 import com.example.productsStore.presentation.ui.productDetails.ProductDetailsScreen
 import com.example.productsStore.presentation.viewModel.ProductDetailsViewModel
+import com.example.productsstore.R
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
@@ -17,6 +22,7 @@ fun ProductDetailsRoute(
 ) {
     val viewModel: ProductDetailsViewModel = hiltViewModel()
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val context = LocalContext.current
 
     LaunchedEffect(viewModel) {
         viewModel.news.collectLatest { news ->
@@ -26,7 +32,14 @@ fun ProductDetailsRoute(
                 }
 
                 is ProductDetailsNews.ShowMessage -> {
-
+                    val message = news.message
+                    if (message is ProductDetailsMessage.Raw) {
+                        Toast.makeText(
+                            context,
+                            news.message.asString(context),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
             }
         }
@@ -38,4 +51,20 @@ fun ProductDetailsRoute(
             viewModel.acceptIntent(intent)
         },
     )
+}
+
+private fun ProductDetailsMessage.asString(context: Context): String {
+    return when (this) {
+        ProductDetailsMessage.ProductNotLoadedYet -> {
+            context.getString(R.string.product_not_loaded_yet)
+        }
+
+        ProductDetailsMessage.ProductAddedToCart -> {
+            context.getString(R.string.product_added_to_cart)
+        }
+
+        is ProductDetailsMessage.Raw -> {
+            message
+        }
+    }
 }
