7fcf555b5724686a58a110a59d8abb5593a43786
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-06-05 18:09:25 +0300

    ADD: CartMessage to avoid hardcoded messages

--- app/src/main/java/com/example/productsStore/presentation/navigation/CartRoute.kt
+++ app/src/main/java/com/example/productsStore/presentation/navigation/CartRoute.kt
@@ -1,13 +1,18 @@
 package com.example.productsStore.presentation.navigation
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.productsStore.presentation.elm.cart.CartMessage
 import com.example.productsStore.presentation.elm.cart.CartNews
 import com.example.productsStore.presentation.ui.cart.CartScreen
 import com.example.productsStore.presentation.viewModel.CartViewModel
+import com.example.productsstore.R
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
@@ -17,6 +22,7 @@ fun CartRoute(
 ) {
     val viewModel: CartViewModel = hiltViewModel()
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val context = LocalContext.current
 
     LaunchedEffect(viewModel) {
         viewModel.news.collectLatest { news ->
@@ -30,7 +36,11 @@ fun CartRoute(
                 }
 
                 is CartNews.ShowMessage -> {
-                    // TODO: добавить показ сообщений
+                    Toast.makeText(
+                        context,
+                        news.message.asString(context),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
@@ -42,4 +52,20 @@ fun CartRoute(
             viewModel.acceptIntent(intent)
         }
     )
+}
+
+private fun CartMessage.asString(context: Context): String {
+    return when (this) {
+        CartMessage.CartCleared -> {
+            context.getString(R.string.cart_cleared)
+        }
+
+        CartMessage.ProductNotFound -> {
+            context.getString(R.string.product_not_found)
+        }
+
+        is CartMessage.Raw -> {
+            message
+        }
+    }
 }
